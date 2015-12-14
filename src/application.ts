58ca55194c77e39d8f6a645fe5ea22355da49855
Wassim Chegham <maneki.nekko@gmail.com>
2015-12-15 00:16:57 +0100

    improve the exposed configuration

--- src/application.ts
+++ src/application.ts
@@ -11,10 +11,40 @@ export namespace App {
 	export let run = (options: any) => {
 			
 		let files = [];
-		if(options.tsconfig && options.tsconfig.files) {
-			files = options.tsconfig.files;
+		if(options.file) {
+			if(
+				!fs.existsSync(options.file) ||
+				!fs.existsSync(path.join(process.cwd(), options.file))
+			) {
+				logger.fatal(`"${ options.file }" file wa not found`);
+				logger.fatal('abort');
+				process.exit(1);
+			}
+			else {
+				files = [options.file];
+			}
+		}
+		else if(options.tsconfig) {
+			if(!fs.existsSync(options.tsconfig)) {
+				logger.fatal('"tsconfig.json" file wa not found in the current directory');
+				logger.fatal('abort');
+				process.exit(1);
+			}
+			else {
+				logger.info('using tsconfig', options.tsconfig);
+				files = require(options.tsconfig).files;
+				
+				if(!files) {
+					logger.fatal('"tsconfig.json" file does not export a "files" attriutes');
+					logger.fatal('abort');
+					process.exit(1);
+				}
+			}
+			
 		}
 		else if (options.files) {
+			logger.info('using files', options.files.length, 'file(s) found');
+			
 			files = options.files;
 		}
 		else {
@@ -22,11 +52,17 @@ export namespace App {
 			logger.fatal('abort');
 			process.exit(1);
 		}
+		
 			
 		let crawler = new Crawler.Dependencies(
 			files.map( file => path.resolve(__dirname, '../', file))
 		);
 		let deps = crawler.getDependencies();
+		
+		if(deps.length <= 0) {
+			logger.info('No dependencies found. See ya');
+			process.exit(0);
+		} 
 		
 		let engine = new Engine.Dot({
 			output: options.output
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import {App} from './application';
 
 App.run({
 	
+	// has more precedence than "tsconfig"
+	file: process.argv[2],
+	
 	// has more precedence than "files"
-	//tsconfig: require('../tsconfig.json'),
+	tsconfig: path.join(process.cwd(), '/tsconfig.json'),
 	
-	// optional
-	files: [path.join(__dirname, '../test/demo-app/app-1/app.ts')],
+	// last option to provide a list of ts files
+	//files: [path.join(process.cwd(), '/test/demo-app/app-1/app.ts')],
 	
-	// launch/view generated file
-	open: false,
+	// launch/view generated file (in browser or OS specific)
+	open: true,
 	
 	//output: {}
 	
