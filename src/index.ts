c37cf5115b1f087243cf1a438ad302392193267d
Wassim Chegham <github@wassimchegham.com>
2016-03-10 22:50:43 +0100

    chore(platform): move the current bootstraping logic into a Nodejs platform folder

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
 /// <reference path="../typings/main.d.ts"/>
-import * as path from 'path';
-import {App} from './application';
+import {Application} from './platform/node/application';
 
-App.run({
-
-	// has more precedence than "tsconfig"
-	file: process.argv[2],
-
-	// has more precedence than "files"
-	tsconfig: path.join('./tsconfig.json'),
-
-	// last option to provide a list of ts files
-	//files: [path.join(process.cwd(), '/test/demo-app/app-1/app.ts')],
-
-	// launch/view generated file (in browser or OS specific)
-	open: true,
-
-	// where to store the generated files
-	output: `./report/${require('../package.json').shortName}`
-
-});
+Application.run();
---
+++ src/platform/node/application.ts
@@ -0,0 +1,101 @@
+/// <reference path="../../../typings/main.d.ts"/>
+import * as ts from 'typescript';
+import * as fs from 'fs';
+import * as path from 'path';
+import {Engine} from '../../engines/dot';
+import {Crawler} from '../../crawlers/dependencies';
+import {logger} from '../../logger';
+
+let pkg = require('../../../package.json');
+let program = require('commander');
+
+export namespace Application {
+
+  program
+    .version(pkg.version)
+      .option('-f, --file [file]', 'Entry *.ts file')
+      .option('-t, --tsconfig [config]', 'A tsconfig.json', './tsconfig.json')
+      .option('-l, --files [list]', 'A list of *.ts files')
+      .option('-o, --open', 'Open the generated diagram file', true)
+      .option('-d, --output [folder]', 'Where to store the generated files', `./documentation/${pkg.name}`)
+      .parse(process.argv);
+
+  let outputHelp = () => {
+    program.outputHelp()
+    process.exit(1);
+  }
+
+  export let run = () => {
+
+    let files = [];
+    if(program.file) {
+      if(
+        !fs.existsSync(program.file) ||
+        !fs.existsSync(path.join(process.cwd(), program.file))
+      ) {
+        logger.fatal(`"${ program.file }" file wa not found`);
+        process.exit(1);
+      }
+      else {
+        files = [program.file];
+      }
+    }
+    else if(program.tsconfig) {
+
+      if(!fs.existsSync(program.tsconfig)) {
+        logger.fatal('"tsconfig.json" file wa not found in the current directory');
+        process.exit(1);
+      }
+      else {
+        program.tsconfig = path.join(
+          path.join(process.cwd(), path.dirname(program.tsconfig)),
+          path.basename(program.tsconfig)
+        );
+        logger.info('using tsconfig', program.tsconfig);
+        files = require(program.tsconfig).files;
+
+        if(!files) {
+          logger.fatal('"tsconfig.json" file does not export a "files" attriute');
+          process.exit(1);
+        }
+      }
+
+    }
+    else if (program.files) {
+      logger.info('using files', program.files.length, 'file(s) found');
+      files = program.files;
+    }
+    else {
+      outputHelp()
+    }
+
+    let crawler = new Crawler.Dependencies(
+      files
+    );
+
+    let deps = crawler.getDependencies();
+
+    if(deps.length <= 0) {
+      logger.info('No dependencies found');
+      process.exit(0);
+    }
+
+    let engine = new Engine.Dot({
+      output: program.output
+    });
+    engine
+      .generateGraph(deps)
+      .then( file => {
+
+        if (program.open === true) {
+          logger.info('openning file ', file);
+          let open = require("opener");
+          open(file);
+        }
+        
+      })
+      .catch( e => logger.error(e) )
+      .finally( _ => logger.info('done'))
+
+  }
+}
